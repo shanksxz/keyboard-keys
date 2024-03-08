acee12ae85a29436bd36c8fbd97b3ccd982292c8
Somya Bhatt <somyacu1@gmail.com>
2024-03-09 02:29:27 +0530

    :rmv key-visual

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,8 @@ type KeyboardProps =  {
   onKeyRelease?: (key: string) => void;
 }
 
-const keys: string[][] = [
-  ['Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Prt Sc', 'Scr Lk', 'Pause'],
-  ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace', 'Insert', 'Home', 'Pg Up', 'Num Lk', '/', '*', '-'],
-  ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Delete', 'End', 'Pg Dn', '7', '8', '9', '+'],
-  ['Caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', "'", 'Enter', '4', '5', '6', ','],
-  ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift', '↑', '1', '2', '3', 'Enter'],
-  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Win', 'Ctrl', '←', '↓', '→', '0', '.']
-];
-
 const App: React.FC<KeyboardProps> = ({ onKeyPress, onKeyRelease }) => {
-  const [activeKeys, setActiveKeys] = useState<string[]>(['0_0']);
+  const [activeKeys, setActiveKeys] = useState<string[]>(['0_0'])
   const keyboardRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -32,7 +23,7 @@ const App: React.FC<KeyboardProps> = ({ onKeyPress, onKeyRelease }) => {
     const handleKeyUp = (e: KeyboardEvent) => {
       e.preventDefault();
       const key = e.code;
-      setActiveKeys((prevKeys) => prevKeys.filter((k) => k !== key));
+      setActiveKeys((prev) => prev.filter((k) => k !== key));
       console.log(`Key Up: ${key}`);
       if (onKeyRelease) {
         onKeyRelease(key);
@@ -41,6 +32,7 @@ const App: React.FC<KeyboardProps> = ({ onKeyPress, onKeyRelease }) => {
     
 
     const keyboard = keyboardRef.current;
+    console.log("Keyboard",keyboard);
     if (keyboard) {
       keyboard.addEventListener('keydown', handleKeyDown);
       keyboard.addEventListener('keyup', handleKeyUp);
@@ -56,34 +48,19 @@ const App: React.FC<KeyboardProps> = ({ onKeyPress, onKeyRelease }) => {
 
   return (
     <div
-      ref={keyboardRef}
-      className="h-screen flex flex-col justify-center items-center"
-      tabIndex={0}
-    >
-      <h1 className="mb-2">
-        You Pressed 
-        <span className='bg-gray-300 px-2 py-1 rounded-sm mx-2'>
-          {activeKeys} 
-        </span>
-        Last
-      </h1>
-      {keys.map((row, i) => (
-        <div key={i} className="flex">
-          {row.map((key, j) => (
-            <div
-              key={j}
-              className={`h-10 border border-black text-center ${
-                key.length > 2 ? 'w-28' : 'w-14'
-              } hover:cursor-pointer ${
-                activeKeys.includes(key) ? 'bg-gray-300' : ''
-              }`}
-            >
-              {key}
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+    ref={keyboardRef}
+    className="h-screen bg-black text-white flex flex-col justify-center items-center"
+    tabIndex={0}
+   >
+    <h1 className="mb-2">
+     You Pressed
+     <span className='bg-gray-700 text-white px-2 py-1 rounded-md mx-2'>
+      {Array.from(activeKeys)}
+     </span>
+      Last
+    </h1>
+   </div>
+ 
   );
 };
 
